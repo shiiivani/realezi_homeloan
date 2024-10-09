2524c9d820c4cac0a4c5225c19b0d2fd0a0606c3
Your Name <you@example.com>
2024-10-09 13:33:43 +0530

    Deleted unused files

--- navbar.js
+++
@@ -1,9 +0,0 @@
-const listItems = document.querySelectorAll(".upper-nav li");
-
-listItems.forEach((item) => {
-  item.addEventListener("click", function () {
-    listItems.forEach((li) => li.classList.remove("active"));
-
-    this.classList.add("active");
-  });
-});
